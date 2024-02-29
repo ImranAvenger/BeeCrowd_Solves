3a58edf6c6dc9d2dc2c2270a32c7e6dd741c397b
Al Imran <max.imran2001@gmail.com>
2024-03-01 00:47:11 +0600

    beecrowd | 1234 -> solved

---
+++ CPP/1234.cpp
@@ -0,0 +1,32 @@
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main() {
+    string line;
+
+    while(getline(cin, line)) {
+        string new_string;
+        int count = 0;
+        for(int i = 0; line[i] != '\0'; i++) {
+            if(line[i] == ' ') {
+                cout << ' ';
+            }
+            else {
+                count++;
+                if(count % 2 != 0) {
+                    cout << toupper(line[i]);
+                }
+                    
+                else {
+                    cout << tolower(line[i]);
+                }
+                    
+            }
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
